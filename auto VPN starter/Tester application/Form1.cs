9a4f072a441c841d340a84a02c6044c926d44739
admin <admin@0208a1a4-cf8c-4ad2-b5c5-b8623831e1cb>
2015-09-13 21:21:41 +0000

    -0.0.1 Initial Commit
    git-svn-id: http://192.168.2.176/svn/csharp/auto_vpn_starter@1 0208a1a4-cf8c-4ad2-b5c5-b8623831e1cb

---
+++ auto VPN starter/Tester application/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Management;
+using System.ServiceProcess;
+using Microsoft.Win32;
+
+namespace Tester_application
+{
+
+    public partial class Form1 : Form
+    {
+        public System.Management.ManagementEventWatcher StartWtch;
+        public System.Management.ManagementEventWatcher StopWtch;
+        public Thread MainThread;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainThread = new Thread(Processmonitor);
+            MainThread.Start();
+        }
+
+        private void Processmonitor()
+        {
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+            StartWtch = new ManagementEventWatcher("Select * From Win32_ProcessStartTrace");
+            StopWtch = new ManagementEventWatcher("Select * From Win32_ProcessStopTrace");
+            StartWtch.EventArrived += mgmtWtch_EventArrived;
+            StopWtch.EventArrived += mgmtWtch_EventArrived;
+            StopWtch.Start();
+            StartWtch.Start();
+        }
+
+        void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            ServiceController service = new ServiceController("OpenVPNService", "Localhost");
+            if (service.Status != ServiceControllerStatus.Running && service.Status != ServiceControllerStatus.StartPending)
+            {
+                service.Start();
+            }
+        }
+
+        private bool CurrentlyRunning(string sProcessName)
+        {
+            //get a list of all running processes on current system
+            Process[] Processes = Process.GetProcesses();
+
+            //Iterate to every process to check if it is out required process
+            foreach (Process SingleProcess in Processes)
+            {
+
+                if (SingleProcess.ProcessName.ToLower().Contains(sProcessName))
+                {
+                    //process found 
+                    return true;
+                }
+            }
+
+            //Process not found
+            return false;
+        }
+        public static TimeSpan GetUptime()
+        {
+            ManagementObject mo = new ManagementObject(@"\\.\root\cimv2:Win32_OperatingSystem=@");
+            DateTime lastBootUp = ManagementDateTimeConverter.ToDateTime(mo["LastBootUpTime"].ToString());
+            return DateTime.Now.ToUniversalTime() - lastBootUp.ToUniversalTime();
+        }
+
+        public static TimeSpan GetUptimeUser()
+        {
+            ManagementScope ms = new ManagementScope("\\root\\cimv2");
+
+            ObjectQuery oq = new ObjectQuery("Select * from Win32_Session");
+
+            ManagementObjectSearcher query = new ManagementObjectSearcher(ms, oq);
+
+            ManagementObjectCollection queryCollection = query.Get();
+
+            DateTime lastBootUp = DateTime.Now.ToUniversalTime();
+            foreach (ManagementObject mo in queryCollection)
+            {
+
+                if (mo["LogonType"].ToString().Equals("2"))
+                //  2 - for logged on User
+                {
+                   lastBootUp = ManagementDateTimeConverter.ToDateTime(mo["StartTime"].ToString());
+                }
+
+            }
+            return DateTime.Now.ToUniversalTime() - lastBootUp.ToUniversalTime();
+        }
+
+        void mgmtWtch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            TimeSpan minuptime = new TimeSpan(0, 10, 0);
+            if (CurrentlyRunning("kodi") || GetUptime() < minuptime || GetUptimeUser() < minuptime)
+            {
+                ServiceController service = new ServiceController("OpenVPNService", "Localhost");
+                if (service.Status != ServiceControllerStatus.Running && service.Status != ServiceControllerStatus.StartPending)
+                {
+                    service.Start();
+                }
+            }
+            else
+            {
+                ServiceController service = new ServiceController("OpenVPNService", "Localhost");
+                if (service.Status != ServiceControllerStatus.Stopped && service.Status != ServiceControllerStatus.StopPending)
+                {
+                    service.Stop();
+                }
+            }
+
+        }
+    }
+}
---
+++ auto VPN starter/auto VPN starter/Service1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Management;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace auto_VPN_starter
+{
+    public partial class Service1 : ServiceBase
+    {
+        public System.Management.ManagementEventWatcher StartWtch;
+        public System.Management.ManagementEventWatcher StopWtch;
+        public Thread MainThread;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            MainThread = new Thread(Processmonitor);
+            MainThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            MainThread.Abort();
+        }
+
+        private void Processmonitor()
+        {
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+            StartWtch = new ManagementEventWatcher("Select * From Win32_ProcessStartTrace");
+            StopWtch = new ManagementEventWatcher("Select * From Win32_ProcessStopTrace");
+            StartWtch.EventArrived += mgmtWtch_EventArrived;
+            StopWtch.EventArrived += mgmtWtch_EventArrived;
+            StopWtch.Start();
+            StartWtch.Start();
+        }
+
+        void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            ServiceController service = new ServiceController("OpenVPNService", "Localhost");
+            if (service.Status != ServiceControllerStatus.Running && service.Status != ServiceControllerStatus.StartPending)
+            {
+                service.Start();
+            }
+        }
+
+        private bool CurrentlyRunning(string sProcessName)
+        {
+            Process[] Processes = Process.GetProcesses();
+            foreach (Process SingleProcess in Processes)
+            {
+                if (SingleProcess.ProcessName.ToLower().Contains(sProcessName))
+                { 
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static TimeSpan GetUptimeUser()
+        {
+            ManagementScope ms = new ManagementScope("\\root\\cimv2");
+            ObjectQuery oq = new ObjectQuery("Select * from Win32_Session");
+            ManagementObjectSearcher query = new ManagementObjectSearcher(ms, oq);
+
+            ManagementObjectCollection queryCollection = query.Get();
+            DateTime lastBootUp = DateTime.Now.ToUniversalTime();
+            foreach (ManagementObject mo in queryCollection)
+            {
+                if (mo["LogonType"].ToString().Equals("2")) //  2 - for logged on User
+                {
+                    lastBootUp = ManagementDateTimeConverter.ToDateTime(mo["StartTime"].ToString());
+                }
+            }
+            return DateTime.Now.ToUniversalTime() - lastBootUp.ToUniversalTime();
+        }
+
+        public static TimeSpan GetUptime()
+        {
+            ManagementObject mo = new ManagementObject(@"\\.\root\cimv2:Win32_OperatingSystem=@");
+            DateTime lastBootUp = ManagementDateTimeConverter.ToDateTime(mo["LastBootUpTime"].ToString());
+            return DateTime.Now.ToUniversalTime() - lastBootUp.ToUniversalTime();
+        }
+
+        void mgmtWtch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            TimeSpan minuptime = new TimeSpan(0,10,0);
+            if (CurrentlyRunning("kodi") || GetUptime() < minuptime || GetUptimeUser() < minuptime)
+            {
+                ServiceController service = new ServiceController("OpenVPNService", "Localhost");
+                if (service.Status != ServiceControllerStatus.Running && service.Status != ServiceControllerStatus.StartPending)
+                {
+                    service.Start();
+                }
+            }
+            else
+            {
+                ServiceController service = new ServiceController("OpenVPNService", "Localhost");
+                if (service.Status != ServiceControllerStatus.Stopped && service.Status != ServiceControllerStatus.StopPending)
+                {
+                    service.Stop();
+                }
+            }
+
+        }
+    }
+}
